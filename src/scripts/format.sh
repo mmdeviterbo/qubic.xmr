6881280501e785d97a82444f36c3baa7259f5579
marty <martydeviterbo@gmail.com>
2025-05-18 11:24:26 +0800

    Add prettier

---
+++ src/scripts/format.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+npx prettier . --write \
+  "!.prettierrc" \
+  "!**/global.css" \
+  "!**/package-lock.json" \
+  "!**/package.json" \
+  "!**/postcss.config.mjs" \
+  "!**/tsconfig.json" \
+  "!**/next-env.d.ts" \
+  "!**/next.config.ts" \
+
