f2a28c4d3f9b0d3d33f7cf2e51f20aef97719920
marty <martydeviterbo@gmail.com>
2025-05-29 05:57:47 +0800

    Add deep dive section

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,21 @@
-import { type FC, memo } from "react";
+import { type FC, memo, useCallback } from "react";
 import Link from "next/link";
 import { QUBIC_XMR_STATS_URL } from "@/utils/constants";
 
 const Footer: FC = () => {
+  const openMultipleTabs = useCallback(() => {
+    const urls = ["https://qubic-xmr-stats.streamlit.app"];
+    urls.forEach((url) => {
+      window.open(url, "_blank", "noopener,noreferrer");
+    });
+  }, []);
+
   return (
     <div className="flex mt-4 gap-4 text-gray-50 text-xs underline">
       <Link href={QUBIC_XMR_STATS_URL.replace("/stats", "")}>Live data</Link>
+      <Link href={""} onClick={openMultipleTabs}>
+        Dive Deeper
+      </Link>
     </div>
   );
 };
