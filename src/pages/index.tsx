21c519b8eb88b72e1774b065d307495bdc53482d
marty <martydeviterbo@gmail.com>
2025-06-01 10:24:05 +0800

    Improve API revalidations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,7 +40,7 @@ const Main: NextPage<{
     async () => (await fetch(CALCULATED_MINING_STATS_URL)).json(),
     {
       refreshInterval: 90000,
-      // focusThrottleInterval: 45000,
+      focusThrottleInterval: 90000,
       fallbackData: miningStatsProps,
     },
   );
