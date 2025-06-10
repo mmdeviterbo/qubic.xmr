0a2ab49fb9c85c2a4b3f179758a527a8be9125ea
marty <martydeviterbo@gmail.com>
2025-06-10 16:14:28 +0800

    Add adsense script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Component {...pageProps} />
       {/* <Analytics /> */}
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1941263261411851" crossOrigin="anonymous" />
     </>
   );
 }
