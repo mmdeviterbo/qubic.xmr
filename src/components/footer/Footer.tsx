2dc86f4140bb24dee1df46e5f55d8f9b51ac92a5
marty <martydeviterbo@gmail.com>
2025-06-14 16:29:57 +0800

    Temp footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -32,6 +32,13 @@ const Footer: FC = () => {
     ];
   }, []);
 
+  // <svg className="w-5 h-5 text-gray-500 transition group-open:rotate-90" xmlns="http://www.w3.org/2000/svg"
+  //     width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+  //     <path fill-rule="evenodd"
+  //         d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z">
+  //     </path>
+  // </svg>
+
   return (
     <div
       className="px-1 md:px-0 flex flex-col md:flex-row gap-2 justify-center mt-6 text-gray-50"
