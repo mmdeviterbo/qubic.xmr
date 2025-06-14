72caed06b5bbac534f6b928dbb4e22dbfef7c257
marty <martydeviterbo@gmail.com>
2025-06-15 00:57:11 +0800

    Fix google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,27 +8,27 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-NL226L3VBQ`}
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-NL226L3VBQ', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
         <title>Qubic Custom Mining</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-NL226L3VBQ`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NL226L3VBQ', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />
       {/* <Analytics /> */}
     </>
