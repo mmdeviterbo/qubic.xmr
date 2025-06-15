72fb90d9859f1fa84e96e61ab038a12195ce38bd
marty <martydeviterbo@gmail.com>
2025-06-15 10:59:47 +0800

    Reset ads

--- src/components/analytics/HorizontalGoogleAds.tsx
+++ src/components/analytics/HorizontalGoogleAds.tsx
@@ -3,28 +3,8 @@ import { type FC } from "react";
 
 const HorizontalGoogleAds: FC = () => {
   return (
-    <div className="mt-8">
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1941263261411851"
-        crossOrigin="anonymous"
-      />
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client="ca-pub-1941263261411851"
-        data-ad-slot="2984328311"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            (adsbygoogle = window.adsbygoogle || []).push({});
-          `,
-        }}
-      />
-    </div>
+    <>
+    </>
   );
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import dynamic from "next/dynamic";
 
-import { GoogleAdsenseScript } from "@/components/analytics/GoogleAdsense";
 import "@/styles/globals.css";
-
-const HorizontalGoogleAds = dynamic(
-  () => import("@/components/analytics/HorizontalGoogleAds"),
-  {
-    ssr: false,
-  },
-);
 
 const GoogleAnalyticsScript = dynamic(
   () => import("@/components/analytics/GoogleAnalytics"),
@@ -27,12 +19,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <GoogleAdsenseScript />
       <GoogleAnalyticsScript />
 
       <Component {...pageProps} />
-
-      <HorizontalGoogleAds />
     </>
   );
 }
