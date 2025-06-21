a8268a6477b785c8a8d9917743fbabdb05411134
marty <martydeviterbo@gmail.com>
2025-06-21 10:56:59 +0800

    Update font of CFB text

--- src/components/main/BlocksChart.tsx
+++ src/components/main/BlocksChart.tsx
@@ -199,7 +199,7 @@ const BlockChart: FC<BlockChartProps> = ({
               },
             ]}
           />
-          <div className="z-1 pl-12 md:pl-16 md:tracking-wider flex items-end opacity-[0.06] h-[45%] absolute text-xs md:text-lg">
+          <div className="z-1 pl-12 md:pl-16 md:tracking-wider flex items-end justify-center gap-1 opacity-[0.06] h-[45%] absolute text-xs md:text-lg">
             Powered by
             <span className="cfb-token-text">$CFB</span>
           </div>
