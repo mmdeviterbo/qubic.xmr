71c4ad2bc3fc98d468bcb2896667b2c0ed332f4e
marty <martydeviterbo@gmail.com>
2025-06-25 14:33:22 +0800

    Update post

--- src/components/common/sponsor/cfb/CfbPost.tsx
+++ src/components/common/sponsor/cfb/CfbPost.tsx
@@ -4,7 +4,7 @@ import { XEmbed } from "react-social-media-embed";
 import useBreakpoints from "@/hooks/useBreakpoints";
 import Modal from "../../Modal";
 
-const isCfbPostShownId = "is-cfb-post-shown-key";
+const isCfbPostShownId = "is-cfb-deposit-post-shown-key";
 
 const CfbPost: FC = () => {
   const { isLg } = useBreakpoints();
@@ -30,12 +30,12 @@ const CfbPost: FC = () => {
   return (
     <Modal show={open} setShow={setOpen}>
       <XEmbed
-        url="https://x.com/c_f_b_token/status/1937417135313072364"
+        url="https://x.com/c_f_b_token/status/1937746120660099305"
         width={isLg ? 480 : 320}
         style={{
           backgroundColor: "white",
           zIndex: 100,
-          height: isLg ? "80vh" : "70vh",
+          // height: isLg ? "80vh" : "70vh",
         }}
       />
       <div className="flex flex-col items-center gap-4">
