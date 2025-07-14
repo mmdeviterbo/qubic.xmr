0ebd05e845ae40ea83999c70673cb20fae5b559f
marty <martydeviterbo@gmail.com>
2025-07-14 22:05:50 +0800

    Correct grammat

--- src/components/common/sponsor/cfb/CfbToken.tsx
+++ src/components/common/sponsor/cfb/CfbToken.tsx
@@ -39,8 +39,8 @@ const CfbToken: FC<{ customClass?: string }> = ({
       >
         <div className="relative">
           <div className="wiggle z-100 italic bg-white border border-gray-300 rounded-lg p-2 md:p-4 w-fit text-gray-800 text-xs md:text-lg">
-              Thanks on the <br /> free stress-test!
-              <div className="absolute top-1/2 -right-2 transform -translate-y-1/2 w-4 h-4 bg-white border-t border-r border-gray-300 rotate-45"></div>
+            Thanks for the <br /> free stress-test!
+            <div className="absolute top-1/2 -right-2 transform -translate-y-1/2 w-4 h-4 bg-white border-t border-r border-gray-300 rotate-45"></div>
           </div>
         </div>
         <Image
